01d02644e27078de96ae657c65553c52ded89147
Brett Mosley <brettmosley@hotmail.com>
2021-04-12 13:55:16 +0000

    Updated teched.spec.js

--- tests/teched.spec.js
+++ tests/teched.spec.js
@@ -1,1 +1,17 @@
- 
+ require('../pages/home');
+require('../pages/product');
+
+describe("teched", function () {
+	it("Should validate the home screen", function () {
+        Then.onTheHomePage.iShouldSeeAllCategories();
+    });
+    it("Should search for a product", function () {
+        When.onTheHomePage.iSearchForProduct();
+        Then.onTheHomePage.theProductListShouldBeFiltered();
+    });
+    it("Should navigate to the product", function () {
+        When.onTheHomePage.iSelectTheFirstProduct();
+        Then.onTheProductPage.theProductTitleIsShown();
+        Then.onTheProductPage.theProductCouldBeOrdered();
+    });
+});
