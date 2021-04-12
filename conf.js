ec0b984bf326d856e7c14cd3d4290ef7e4ee63da
Brett Mosley <brettmosley@hotmail.com>
2021-04-12 13:13:32 +0000

    Updated conf.js

--- conf.js
+++ conf.js
@@ -1,4 +1,11 @@
 exports.config = {
     profile: "integration",
     baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",  
+    // specs: "./tests/*.spec.js",
+    reporters: [{
+        name: './reporter/junitReporter',
+        reportName: 'target/report/myReport.xml',
+        prefix: 'mySuitePrefix',
+        postfix: 'mySuitePostfix'
+    }]
 }
