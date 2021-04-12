cfb8d8a65c974595b6bc48d38e006bf0c51b9f00
Brett Mosley <brettmosley@hotmail.com>
2021-04-12 16:37:50 +0000

    Updated teched.spec.js

--- tests/teched.spec.js
+++ tests/teched.spec.js
@@ -5,6 +5,8 @@ describe("teched", function () {
 	it("Should validate the home screen", function () {
         Then.onTheHomePage.iShouldSeeAllCategories();
         console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        console.log("---"+browser.testrunner.config.params.user)
+        console.log("---"+browser.testrunner.config.params.password)
     });
     it("Should search for a product", function () {
         When.onTheHomePage.iSearchForProduct();
